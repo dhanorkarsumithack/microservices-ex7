5a907bf36cdc50982cdab95161cb689e58da24d7
sumitd <sumit.dhanorkar@neml.in>
2023-06-14 14:57:22 +0530

    circuit breaker is added in the orderservice

--- order-service/src/main/java/com/sumit/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/sumit/orderservice/controller/OrderController.java
@@ -2,6 +2,9 @@ package com.sumit.orderservice.controller;
 
 import com.sumit.orderservice.dto.OrderRequest;
 import com.sumit.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,11 +18,18 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory",fallbackMethod = "fallbackMethod")
+//    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
     public String placeOrder(@RequestBody OrderRequest orderRequest){
         orderService.placeOrder(orderRequest);
         return "Order placed successfully...";
     }
 
+
+    public String fallbackMethod(OrderRequest orderRequest,RuntimeException runtimeException){
+        return "something happened wrong...";
+    }
 
 
 }
